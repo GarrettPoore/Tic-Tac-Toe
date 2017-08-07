87b4aa702c01a829d49f0eef66f4c349654b9535
GarrettPoore <garrettjpoore@gmail.com>
2017-08-06 20:55:10 -0400

    Create a function to find all winning moves for a player (now can pick from multiple wins/blocks and will be easier for fork calculating)

--- ai.js
+++ ai.js
@@ -2,55 +2,46 @@ var CORNERS = [0, 2, 6, 8]
 
 //AI Functions
 function aiPlay() {
-  var availableSpaces = getAvailableSpaces();
-
   //Order of possible plays taken from Wikipedia:
   //https://en.wikipedia.org/wiki/Tic-tac-toe#Strategy
-  if (aiWin(availableSpaces)) {}
-  else if (aiBlock(availableSpaces)) {}
-  else if (aiFork(availableSpaces)) {}
-  else if (aiBlockFork(availableSpaces)) {}
-  else if (aiCenter(availableSpaces)) {}
-  else if (aiOpposingCorner(availableSpaces)) {}
-  else if (aiCorner(availableSpaces)) {}
-  else {aiDefault(availableSpaces);}
+  if (aiWin()) {}
+  else if (aiBlock()) {}
+  else if (aiFork()) {}
+  else if (aiBlockFork()) {}
+  else if (aiCenter()) {}
+  else if (aiOpposingCorner()) {}
+  else if (aiCorner()) {}
+  else {aiDefault();}
   endTurn();
 }
 
 //All of the below functions will try to complete an action for the ai
 //and return true if they did, or false if they did nothing
 
-function aiWin(spaces) {
-  for (var i = 0; i < spaces.length; i++) {
-    var copyBaord = getBoardCopy();
-    var space = spaces[i];
-
-    copyBaord[space] = data.player2;
-    if (checkForWinner(copyBaord, data.player2)) {
-      console.log("Winning at index " + space);
-      placeMoveByIndex(space);
-      return true;
-    }
+function aiWin() {
+  var moves = getWinningMoves(data.player2);
+  if (moves.length > 0) {
+    var move = pickRandomSpace(moves);
+    console.log("Winning at index " + move);
+    placeMoveByIndex(move);
+    return true;
   }
   return false;
 }
 
-function aiBlock(spaces) {
-  for (var i = 0; i < spaces.length; i++) {
-    var copyBaord = getBoardCopy();
-    var space = spaces[i];
-
-    copyBaord[space] = data.player1;
-    if (checkForWinner(copyBaord, data.player1)) {
-      console.log("Blocking at index " + space);
-      placeMoveByIndex(space);
-      return true;
-    }
+function aiBlock() {
+  var moves = getWinningMoves(data.player1);
+  if (moves.length > 0) {
+    var move = pickRandomSpace(moves);
+    console.log("Blocking at index " + move);
+    placeMoveByIndex(move);
+    return true;
   }
   return false;
 }
 
-function aiFork(spaces) {
+function aiFork() {
+  var spaces = getAvailableSpaces();
   for (var i = 0; i < spaces.length; i++) {
     var copyBaord = getBoardCopy();
     var space = spaces[i];
@@ -60,7 +51,8 @@ function aiFork(spaces) {
   return false;
 }
 
-function aiBlockFork(spaces) {
+function aiBlockFork() {
+  var spaces = getAvailableSpaces();
   for (var i = 0; i < spaces.length; i++) {
     var copyBaord = getBoardCopy();
     var space = spaces[i];
@@ -71,9 +63,10 @@ function aiBlockFork(spaces) {
 }
 
 //Place in the center, if available
-function aiCenter(spaces) {
+function aiCenter() {
+  var spaces = getAvailableSpaces();
   var CENTER = 4;
-  
+
   if (spaces.includes(CENTER)) {
     console.log("Center at index " + CENTER);
     placeMoveByIndex(CENTER);
@@ -83,7 +76,8 @@ function aiCenter(spaces) {
 }
 
 //Place in a corner that is opposite the enemy
-function aiOpposingCorner(spaces) {
+function aiOpposingCorner() {
+  var spaces = getAvailableSpaces();
   var enemyCorners = [];
   data.board.forEach(function(move, space){
     if (CORNERS.includes(space) && move == data.player1) {
@@ -119,13 +113,13 @@ function aiOpposingCorner(spaces) {
 }
 
 //Take any open corner
-function aiCorner(spaces) {
-  var corners = spaces.filter(function(space){
+function aiCorner() {
+  var corners = getAvailableSpaces().filter(function(space){
     return CORNERS.includes(space);
   });
 
   if (corners.length > 0) {
-    var space = corners[Math.floor(Math.random() * corners.length)];
+    var space = pickRandomSpace(corners);
 
     console.log("Corner at index " + space);
     placeMoveByIndex(space);
@@ -135,9 +129,32 @@ function aiCorner(spaces) {
 }
 
 //Default to any open space
-function aiDefault(spaces) {
-  var space = spaces[Math.floor(Math.random() * spaces.length)];
+function aiDefault() {
+  var space = pickRandomSpace(getAvailableSpaces());
   console.log("Defaulting at index " + space);
   placeMoveByIndex(space);
   return true;
+}
+
+//Other Functions
+
+//Get all the winning moves for a specific player
+function getWinningMoves(player) {
+  var spaces = getAvailableSpaces();
+  var moves = [];
+  for (var i = 0; i < spaces.length; i++) {
+    var copyBaord = getBoardCopy();
+    var space = spaces[i];
+
+    copyBaord[space] = player;
+    if (checkForWinner(copyBaord, player)) {
+      moves.push(space);
+    }
+  }
+  return moves;
+}
+
+//Returns a random spaces from an array of spaces
+function pickRandomSpace(spaces) {
+  return spaces[Math.floor(Math.random() * spaces.length)]
 }
